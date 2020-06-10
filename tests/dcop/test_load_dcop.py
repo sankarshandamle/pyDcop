65586b3c6f2555c10c385a7f73b7f934196715d7
Fransman <je.fransman@gmail.com>
2020-06-10 16:39:09 +0200

    UPDATE: dcop testfile tests for constraints

--- tests/dcop/test_load_dcop.py
+++ tests/dcop/test_load_dcop.py
@@ -6,11 +6,16 @@ These tests check for correct parsing of the DCOP definition files.
 
 """
 import unittest
+import pathlib
 from pydcop.dcop.yamldcop import load_dcop_from_file, load_dcop
 from pydcop.dcop.objects import (
     ContinuousDomain,
     Domain,
 )
+from pydcop.dcop.relations import (
+    NAryFunctionRelation,
+)
+from pydcop.utils.expressionfunction import ExpressionFunction
 
 dcop_test_str = """
     name: 'dcop test'
@@ -54,9 +59,19 @@ dcop_test_str = """
             initial_value: False
 
     constraints:
-        c1:
+        c1_intention:
             type: intention
             function: var3 - var1
+        c2_intention_source: 
+            type: intention
+            source: "branin_constraint.py"
+            function: source.branin(var1, var2)
+        c3_intention_property: 
+            type: intention
+            source: "bird_constraint.py"
+            function: source.bird(var1, var2)
+            properties:
+                Lipschitz_constant: 1326.5
 
     agents:
         a1:
@@ -70,11 +85,21 @@ dcop_test_str = """
 """
 
 class TestDomains(unittest.TestCase):
+    def load_dcop(self):
+        # Get the path to the tests/instances directory
+        current_dir = pathlib.Path(__file__).parent.absolute()
+        main_dir = current_dir.parent / 'instances'
+
+        # Load the DCOP
+        dcop = load_dcop(
+            dcop_test_str, 
+            main_dir=main_dir,
+        )
+
+        return dcop
 
     def test_classes(self):
-        # Load the DCOP
-        dcop = load_dcop(dcop_test_str)
-        
+        dcop = self.load_dcop()
         # Check the classes
         self.assertIsInstance(dcop.domains['dint'], Domain)
         self.assertIsInstance(dcop.domains['dstr'], Domain)
@@ -82,9 +107,7 @@ class TestDomains(unittest.TestCase):
         self.assertIsInstance(dcop.domains['dbool'], Domain)
 
     def test_values(self):
-        # Load the DCOP
-        dcop = load_dcop(dcop_test_str)
-
+        dcop = self.load_dcop()
         # Check the values
         self.assertEqual(dcop.domains['dint'].values, (0, 1, 2, 3, 4))
         self.assertEqual(dcop.domains['dstr'].values, ('A', 'B', 'C', 'D', 'E'))
@@ -92,9 +115,54 @@ class TestDomains(unittest.TestCase):
         self.assertEqual(dcop.domains['dbool'].values, (True, False))
 
     def test_bounds(self):
-        # Load the DCOP
-        dcop = load_dcop(dcop_test_str)
-
+        dcop = self.load_dcop()
         # Check the bounds
         self.assertEqual(dcop.domains['dcont'].lower_bound, 0.0)
         self.assertEqual(dcop.domains['dcont'].upper_bound, 1.0)
+
+class TestConstraints(unittest.TestCase):
+    def load_dcop(self):
+        # Get the path to the tests/instances directory
+        current_dir = pathlib.Path(__file__).parent.absolute()
+        main_dir = current_dir.parent / 'instances'
+
+        # Load the DCOP
+        dcop = load_dcop(
+            dcop_test_str, 
+            main_dir=main_dir,
+        )
+
+        return dcop
+
+    def test_intentional(self):
+        dcop = self.load_dcop()
+
+        # Check the constraints
+        c1_intention = dcop.constraints['c1_intention']
+        self.assertIsInstance(c1_intention, NAryFunctionRelation)
+        self.assertIsInstance(c1_intention.function, ExpressionFunction)
+        self.assertIn('var1', c1_intention.scope_names)
+        self.assertIn('var3', c1_intention.scope_names)
+        self.assertEqual(c1_intention.expression, 'var3 - var1')
+        self.assertIsNone(c1_intention.function._source_file)
+
+        c2_intention_source = dcop.constraints['c2_intention_source']
+        self.assertIsInstance(c2_intention_source, NAryFunctionRelation)
+        self.assertIsInstance(c2_intention_source.function, ExpressionFunction)
+        self.assertIn('var1', c2_intention_source.scope_names)
+        self.assertIn('var2', c2_intention_source.scope_names)
+        self.assertEqual(c2_intention_source.expression, 'source.branin(var1, var2)')
+
+        current_dir = pathlib.Path(__file__).parent.absolute()
+        main_dir = current_dir.parent / 'instances'
+        self.assertEqual(c2_intention_source.function._source_file, main_dir / 'branin_constraint.py')
+
+        c3_intention_property = dcop.constraints['c3_intention_property']
+        self.assertIsInstance(c3_intention_property, NAryFunctionRelation)
+        self.assertIsInstance(c3_intention_property.function, ExpressionFunction)
+        self.assertIn('var1', c3_intention_property.scope_names)
+        self.assertIn('var2', c3_intention_property.scope_names)
+        self.assertEqual(c3_intention_property.expression, 'source.bird(var1, var2)')
+        self.assertEqual(c3_intention_property._properties, {'Lipschitz_constant': 1326.5})
+
+TestConstraints().test_intentional()
